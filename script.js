712fc24449b8cecc3eafcfc34d381c037fc12755
Alexander Hovy <colorizor@gmail.com>
2015-04-11 13:12:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,8 @@ function highlight() {
   htmlStr = htmlStr.replace(htmlAtt, '<span id="html-att">$&</span>');
   htmlStr = htmlStr.replace(htmlVal, '<span id="html-val">$&</span>');
   htmlStr = htmlStr.replace(htmlFix, '&#62;</span>');
-  htmlStr = htmlStr.replace(/>([\n].*?)</ig, '></li><li$&');
+htmlStr = htmlStr.replace(/([\w\W]+)([\n].*?)</ig, '></li><li$&');
+  //htmlStr = htmlStr.replace(/>([\n].*?)</ig, '></li><li$&');
   htmlStr = htmlStr.replace(/<li>\n/ig, '<li>');
   htmlStr = htmlStr.replace(/([\s\S]+)/ig, '<ol><li>$&</li></ol>');
   document.getElementById('html-pre').innerHTML = htmlStr;
