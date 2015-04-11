118c837bdff268778e4d457404d79c5fe71419ff
Alexander Hovy <colorizor@gmail.com>
2015-04-11 12:27:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,11 @@ function highlight() {
   //By replacing the characters it allowes
   //for not replacing the span tag characters
   var htmlClean = [/&lt;/ig, /&gt;/ig, /[/]/ig, /[=]/ig, /["]/ig, /[!]/ig, /[-]/ig, /[\t]/ig];
-  var htmlReplace = ['&#60;', '&#62;', '&#47;', '&#61;', '&#34;', '&#33;', '&#45;', '&nbsp;&nbsp;&nbsp;&nbsp;'];
+  var htmlReplace = ['&#60;', '&#62;', '&#47;', '&#61;', '&#34;', '&#33;', '&#45;', '\s\s\s\s'];
   //Fixes the colouring of all the > characters
   //at the comments so that the > character
   //at all the comments are the same colour
   var htmlFix = /<span id="html-tag">&#62;<\/span><\/span>/ig;
-  //Fixes the numbering problem by putting
-  //the code that is not in span tags
-  //in span tags
-  var htmlSpan = /<\/span>([\s\S]*?)<span id="/ig;
-  var htmlRemove = [/<span id="html-text"><\/span>/ig, /<span id="<\/span>/ig];
-  var htmlInsert = ['<span id="html-text">', '</span>'];
-  //Fixes the comments that are spread over
-  //more than one line with a new line
-  //somewhere within the comment tag
-  var htmlStitch = /<\/li>([\s\S]*?)/ig;
   //Comment
   var htmlCom = /(&#60;&#33;DOCTYPE|&#60;&#33;&#45;&#45;)([\s\S]*?)(&#45;&#45;&#62;|&#62;)/ig;
   //Tag
@@ -38,10 +28,6 @@ function highlight() {
   htmlStr = htmlStr.replace(htmlAtt, '<span id="html-att">$&</span>');
   htmlStr = htmlStr.replace(htmlVal, '<span id="html-val">$&</span>');
   htmlStr = htmlStr.replace(htmlFix, '&#62;</span>');
-  htmlStr = htmlStr.replace(htmlSpan, '<span id="html-text">$&</span>');
-  for (b = 0; b < htmlRemove.length; b++) {
-    htmlStr = htmlStr.replace(htmlRemove[b], htmlInsert[b]);
-  }
   htmlStr = htmlStr.replace(/\n/ig, '</li><li>');
   htmlStr = htmlStr.replace(/([\s\S]+)/ig, '<ol><li>$&</li></ol>');
   document.getElementById('html-pre').innerHTML = htmlStr;
