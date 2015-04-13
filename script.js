9468985da16ba511f2c3465d2881d80e75a4af89
Alexander Hovy <colorizor@gmail.com>
2015-04-13 22:51:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -108,9 +108,10 @@ function preLoad() {
     for (aa = 0; aa < numLine; aa++) {
       numStr = numStr + '<span id="num-line">' + (aa + 1) + '</span>\n';
     }
+    $(this).wrap('<table><tr id="pre-table"><td id="pre-tag"></td></tr></table>');
     //-------------------------Get Full Pre Tag
-    $(this).wrap(function() {
-      return '<table><tr><td id="num-col">' + numStr + '</td><td>' + preTag + '</td></tr></table>';
-    });
+    //$(this).wrap(function() {
+      //return '<table><tr><td id="num-col">' + numStr + '</td><td>' + preTag + '</td></tr></table>';
+    //});
   });
 }
