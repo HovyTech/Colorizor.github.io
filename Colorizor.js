aca7f0cde9bc1f020e5888895a104c9c978aa250
Alexander Hovy <colorizor@gmail.com>
2015-10-11 21:45:16 +0200

    Update Colorizor.js

--- Colorizor.js
+++ Colorizor.js
@@ -60,7 +60,7 @@ function preLoad() {
     //-------------------------Insert Coloured Text
     $(this).html(htmlStr);
   });
-  $('html-com').css('color', 'yellow');
+  $('#html-com').css('color', 'yellow');
   //--------------------------------------------------CSS
   $.each($('pre[id="css"]'), function() {
     //-------------------------Get Text
