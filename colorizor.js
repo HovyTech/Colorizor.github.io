2dd72f8008001947a60bbdb3030f7a8fe81e3457
Alexander Hovy <colorizor@gmail.com>
2015-10-19 10:11:15 +0200

    Update colorizor.js

--- colorizor.js
+++ colorizor.js
@@ -24,24 +24,17 @@ var jsVal = /([\w]+)(?=\s(.*?\(.*?\).*?\{))/igm;
 //--------------------------------------------------Clean Up--------------------------------------------------
 //------------------------------------------------------------------------------------------------------------
 function preLoad() {
-  //--------------------------------------------------Clean
-  $.each($('pre'), function() {
-    //-------------------------Get Text
-    var cleanStr = $(this).html();
-    
-    for (a = 0; a < clean.length; a++) {
-      cleanStr = cleanStr.replace(clean[a], rep[a]);
-    }
-    
-    cleanStr = cleanStr.replace(link, '<span id="link"><a href="$&">$&</a></span>');
-    
-    $(this).html(cleanStr);
-  });
-  
   //--------------------------------------------------HTML
   $.each($('pre[id="html"]'), function() {
     //-------------------------Get Text
     var htmlStr = $(this).html();
+    
+    htmlStr = htmlStr.replace(link, '<span id="link"><a href="$&">$&</a></span>');
+    
+    for (a = 0; a < clean.length; a++) {
+      htmlStr = htmlStr.replace(clean[a], rep[a]);
+    }
+    
     //-------------------------Wrap Matching Text
     htmlStr = htmlStr.replace(htmlCom, '<span id="html-com">$&</span>');
     htmlStr = htmlStr.replace(htmlTag, '<span id="html-tag">$&</span>');
