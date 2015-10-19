f74e273779b6a1e14f1358f3a7c96a71b7707add
Alexander Hovy <colorizor@gmail.com>
2015-10-19 16:43:29 +0200

    Update colorizor.js

--- colorizor.js
+++ colorizor.js
@@ -8,6 +8,7 @@ var rep = ['&#60;', '&#62;', '&#47;', '&#61;', '&#34;', '&#33;', '&#45;', '\s\s\
 var link = /(ftp|http|https):\/\/([\w0-9±!@#$%ˆ&*()_+§\-=[\]{}:;'|\\,.?/`˜]+)/igm;
 var color = /(rgb|rgba|#)([(0-9a-zA-Z,)].+)(?=(.*?);)/igm;
 var regex = /&#47;(.*?)&#47;([igm]+)/igm;
+var units = /([^\D])([\d.]*?)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm;
 //--------------------------------------------------HTML
 var htmlCom = /(&#60;&#33;&#45;&#45;(.*?)$|(.*?)([\w]+)(?=\n(.*?)&#45;&#45;&#62;)|(.*?)&#45;&#45;&#62;)/igm;
 var htmlTag = /((&#60;&#33;|&#60;|&#60;&#47;)([\w]+)(&#62;|\S|(\s&#47;&#62;|&#47;&#62;))|&#62;)/igm;
@@ -96,6 +97,7 @@ function preLoad() {
     extraStr = extraStr.replace(link, '<a id="link" href="$&" target="_blank">$&</a>');
     extraStr = extraStr.replace(color, '<span style="color: $&;">$&</span>');
     extraStr = extraStr.replace(regex, '<span id="regex">$&</span>');
+    extraStr = extraStr.replace(units, '<span style="color: rgb(150, 150, 150);">$&</span>');
     $(this).html(extraStr);
   });
   
