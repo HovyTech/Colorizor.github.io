9eb98c557eb0ac0bc84f00ae6a225ac96c564578
Alexander Hovy <colorizor@gmail.com>
2015-10-22 16:08:40 +0200

    Update FullScript.js

--- Files/FullScript.js
+++ Files/FullScript.js
@@ -55,21 +55,15 @@ function colorizor() {
 }
 //--------------------------------------------------GET SOCIAL COUNTS  
 function getFacebookCount() {
-  $.getJSON('https://graph.facebook.com/fql?q=SELECT%20like_count,%20total_count,%20share_count,%20click_count,%20comment_count%20FROM%20link_stat%20WHERE%20url%20=%20%22https://www.facebook.com/pages/HovyTech/755340597847731%22', function(data) {
+  $.getJSON('https://graph.facebook.com/fql?q=SELECT%20like_count,%20total_count,%20share_count,%20click_count,%20comment_count%20FROM%20link_stat%20WHERE%20url%20=%20%22https://www.facebook.com/pages/Colorizor/479642585547376%22', function(data) {
     var facebook = data.data[0].total_count;
     $('#facebook-count').text(facebook);
   });
 }
 function getTwitterCount() {
-  $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=https://twitter.com/HovyTech&callback=?', function(data) {
+  $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=https://colorizor.github.io', function(data) {
     var twitter = data.count;
     $('#twitter-count').text(twitter);
-  });
-}
-function getYouTubeCount() {
-  $.getJSON('http://gdata.youtube.com/feeds/api/users/hovytech?alt=json', function(data) {
-    var youtube = data.entry.yt$statistics.subscriberCount;
-    $('#youtube-count').text(youtube);
   });
 }
 function getGooglePlusCount() {
@@ -90,5 +84,17 @@ function getGooglePlusCount() {
   function(data) {
     var googleplus = data.count;
     $('#googleplus-count').text(googleplus);
+  });
+}
+function getPinterestCount() {
+  $.getJSON('https://api.pinterest.com/v1/urls/count.json?callback=&url=https://colorizor.github.io', function(data) {
+    var pinterest = data.count;
+    $('#pinterest-count').text(pinterest);
+  });
+}
+function getLinkedInCount() {
+  $.getJSON('https://www.linkedin.com/countserv/count/share?url=https://colorizor.github.io&format=json', function(data) {
+    var linkedin = data.count;
+    $('#linkedin-count').text(linkedin);
   });
 }
