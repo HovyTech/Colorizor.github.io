6715316f707263f4e1cf08bbf1370ff8a3521468
Alexander Hovy <colorizor@gmail.com>
2015-10-23 09:21:15 +0200

    Update FullScript.js

--- Files/FullScript.js
+++ Files/FullScript.js
@@ -60,40 +60,6 @@ function colorizor() {
       }
     });
   }
-  
-  $('span[id$="all-code"]').click(function() {
-      var range, selection;
-    
-      if (window.getSelection && document.createRange) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents($(this)[0]);
-        selection.removeAllRanges();
-        selection.addRange(range);
-      } else if (document.selection && document.body.createTextRange) {
-        alert('mobile hhjdsjdhjh')
-        range = document.body.createTextRange();
-        range.moveToElementText($(this)[0]);
-        range.select();
-      }
-    });
-  
-  $('span[id$="all-code"]').click(function() {
-      var range, selection;
-    
-      if (window.getSelection && document.createRange) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents($(this)[0]);
-        selection.removeAllRanges();
-        selection.addRange(range);
-      } else {
-        alert('mobile')
-        range = document.body.createTextRange();
-        range.moveToElementText($(this)[0]);
-        range.select();
-      }
-    });
 }
 //--------------------------------------------------GET SOCIAL COUNTS  
 //function getFacebookCount() {
