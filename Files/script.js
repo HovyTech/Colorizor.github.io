6c6eb7f5d0603fe0c636d9ff1152bcc19f534ce5
Alexander Hovy <hovytech@gmail.com>
2015-10-24 23:10:01 +0200

    Original Working Copy

--- Files/script.js
+++ Files/script.js
@@ -1,32 +1,118 @@
-//--------------------------------------------------DETECT LANGUAGE
-$(document).ready(function() {
-  //--------------------------------------------------VARIABLES
-  var link = /(ftp|http|https):\/\/([\w0-9±!@#$%ˆ&*()_+§\-=[\]{}:;'|\\,.?/`˜]+)/igm;
-  var color = /(rgb|rgba|#)([(0-9a-zA-Z,)].+)(?=(.*?);)/igm;
-  var regx = /&#47;(.*?)&#47;([igm]+)/igm;
-  var units = /([^\D])([\d.]*?)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm;
-  
-  var findDupLang = [];
-  
-  //----------------------------------------------FINDING LANGUAGE
-  $.each($('pre'), function() {
-    var language = $(this).attr('language').toLowerCase();
-    
-    findDuplicates.push(language);
-    
-    for (a = 0; a < findDuplicates.length; a++) {
-      if (!(findDuplicates.indexOf(language) > -1)) {
-        loadJS('https://colorizor.github.io/Languages/' + language + '.js');
-      }
-    }
-  });
-  
-  //----------------------------------------------SCREEN SIZE
+//------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------RegEx----------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------Clean
+var clean = [/&lt;/igm, /&gt;/igm, /[/]/igm, /[=]/igm, /["]/igm, /[!]/igm, /[-]/igm, /[\t]/igm];
+var rep = ['&#60;', '&#62;', '&#47;', '&#61;', '&#34;', '&#33;', '&#45;', '\s\s\s\s'];
+//--------------------------------------------------General
+var link = /(ftp|http|https):\/\/([\w0-9±!@#$%ˆ&*()_+§\-=[\]{}:;'|\\,.?/`˜]+)/igm;
+var color = /(rgb|rgba|#)([(0-9a-zA-Z,)].+)(?=(.*?);)/igm;
+var regx = /&#47;(.*?)&#47;([igm]+)/igm;
+var units = /([^\D])([\d.]*?)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm;
+//--------------------------------------------------HTML
+var htmlCom = /(&#60;&#33;&#45;&#45;(.*?)$|(.*?)([\w]+)(?=\n(.*?)&#45;&#45;&#62;)|(.*?)&#45;&#45;&#62;)/igm;
+var htmlTag = /((&#60;&#33;|&#60;|&#60;&#47;)([\w]+)(&#62;|\S|(\s&#47;&#62;|&#47;&#62;))|&#62;)/igm;
+var htmlAtt = /([\S]+)&#61;(?=&#34;([\s\S]*?)&#34;)/igm;
+var htmlVal = /&#34;([\s\S]*?)&#34;/igm;
+var htmlPar = /\s([\w]+)(?=<span)/igm;
+var htmlFixA = /&#45;&#45;<span id="selector">&#62;<\/span>/igm;
+//--------------------------------------------------CSS
+var cssCom = /(&#47;\*(.*?)$|(.*?)([\w]+)(?=\n(.*?)\*&#47;)|(.*?)\*&#47;)/igm;
+var cssSel = /(^|,.+)([\w]+)(?=.+{)/igm;
+var cssSelExt = /:([\w]+)(?=.+{)/igm;
+var cssProp = /(?!.+{)(([\w]|&#45;)+)(?=:(.*?);)/igm;
+var cssVal = /:([\s\S].+);/igm;
+//--------------------------------------------------JS
+var jsCom = /&#47;&#47;.*/igm;
+var jsText = /(&#34;(.*?)&#34;|'(.*?)')/igm;
+var jsSel = /(?!\$\()([\w]+)(?=\)\.)/igm;
+var jsVal = /(([\w]+)\s(?=([\w]+)\(\)(.*?){)|var)/igm;
+var jsChar = /((?!(function)\s)([\w]+)\(\)(?=(.*?){)|((\$|\.([\w]+))(.*?)|([\w]+))\(|(?!(.*?){)\)|\)(?=,))/igm;
+
+//------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------Clean Up--------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
+function colorizor() {
+  //--------------------------------------------------DIFFERENT DEVICES
   if (screen.width < 1024) {
     $('pre').width(screen.width - 40);
+  } else {
+    $('span[id$="all-code"]').click(function() {
+      var range, selection;
+    
+      if (window.getSelection && document.createRange) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents($(this)[0]);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      } else if (document.selection && document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText($(this)[0]);
+        range.select();
+      }
+    });
   }
   
-  //----------------------------------------------FEATURES
+  //--------------------------------------------------HTML
+  $.each($('pre[id="html"]'), function() {
+    //-------------------------Get Text
+    var htmlStr = $(this).html();
+    
+    for (a = 0; a < clean.length; a++) {
+      htmlStr = htmlStr.replace(clean[a], rep[a]);
+    }
+    
+    //-------------------------Wrap Matching Text
+    htmlStr = htmlStr.replace(htmlCom, '<span id="comment">$&</span>');
+    htmlStr = htmlStr.replace(htmlTag, '<span id="selector">$&</span>');
+    htmlStr = htmlStr.replace(htmlAtt, '<span id="attribute">$&</span>');
+    htmlStr = htmlStr.replace(htmlVal, '<span id="value">$&</span>');
+    htmlStr = htmlStr.replace(htmlPar, '<span id="parameter">$&</span>');
+    htmlStr = htmlStr.replace(htmlFixA, '&#45;&#45;&#62;');
+    //-------------------------Insert Coloured Text
+    $(this).html(htmlStr);
+  });
+  
+  //--------------------------------------------------CSS
+  $.each($('pre[id="css"]'), function() {
+    //-------------------------Get Text
+    var cssStr = $(this).html();
+    
+    for (a = 0; a < clean.length; a++) {
+      cssStr = cssStr.replace(clean[a], rep[a]);
+    }
+    
+    //-------------------------Wrap Matching Text
+    cssStr = cssStr.replace(cssCom, '<span id="comment">$&</span>');
+    cssStr = cssStr.replace(cssSel, '<span id="selector">$&</span>');
+    cssStr = cssStr.replace(cssSelExt, '</span><span id="parameter">$&</span>');
+    cssStr = cssStr.replace(cssProp, '<span id="attribute">$&</span>');
+    cssStr = cssStr.replace(cssVal, '<span id="value">$&</span>');
+    //-------------------------Insert Coloured Text
+    $(this).html(cssStr);
+  });
+  
+  //--------------------------------------------------JS
+  $.each($('pre[id="js"]'), function() {
+    //-------------------------Get Text
+    var jsStr = $(this).html();
+    
+    for (a = 0; a < clean.length; a++) {
+      jsStr = jsStr.replace(clean[a], rep[a]);
+    }
+    
+    //-------------------------Wrap Matching Text
+    jsStr = jsStr.replace(jsCom, '<span id="comment">$&</span>');
+    jsStr = jsStr.replace(jsText, '<span id="value">$&</span>');
+    jsStr = jsStr.replace(jsSel, '<span id="parameter">$&</span>');
+    jsStr = jsStr.replace(jsVal, '<span id="attribute">$&</span>');
+    jsStr = jsStr.replace(jsChar, '<span id="selector">$&</span>');
+    //-------------------------Insert Coloured Text
+    $(this).html(jsStr);
+  });
+  
+  //----------------------------------------------URL
   $.each($('pre, span'), function() {
     var extraStr = $(this).html();
     extraStr = extraStr.replace(link, '<a id="link" href="$&" target="_blank">$&</a>');
@@ -36,7 +122,7 @@ $(document).ready(function() {
     $(this).html(extraStr);
   });
   
-  //----------------------------------------------NUMBERING
+  //----------------------------------------------Numbering
   $.each($('pre'), function() {
     var preStr = $(this).html();
     preStr = preStr.replace(/([\s\S]+)/igm, '<span id="all-number"></span><span id="all-code">$&</span>');
@@ -57,7 +143,6 @@ $(document).ready(function() {
     });
   });
   
-  //----------------------------------------------SELECT CODE
   $('span[id="all-code"]').click(function() {
     var range, selection;
     
@@ -73,52 +158,4 @@ $(document).ready(function() {
       range.select();
     }
   });
-});
-
-//--------------------------------------------------LOAD LANGUAGE
-function loadJS(src, cb) {
-  'use strict';
-  var ref = window.document.getElementsByTagName('script')[0];
-  var script = window.document.createElement('script');
-  script.src = src;
-  script.async = true;
-  ref.parentNode.insertBefore(script, ref);
-  if (cb && typeof(cb) === 'function') {
-    script.onload = cb;
-  }
-  return script;
 }
-
-//--------------------------------------------------LOAD THEME
-//function loadCSS(href, before, media, callback) {
-  //'use strict';
-  //var ss = window.document.createElement('link');
-  //var ref = before || window.document.getElementsByTagName('script')[0];
-  //var sheets = window.document.styleSheets;
-  //ss.rel = 'stylesheet';
-  //ss.href = href;
-  //ss.media = 'only x';
-  //if (callback) {
-    //ss.onload = callback;
-  //}
-  //ref.parentNode.insertBefore(ss, ref);
-  //ss.onloadcssdefined = function(cb) {
-    //var defined;
-    //for (var i = 0; i < sheets.length; i++) {
-      //if (sheets[i].href && sheets[i].href.indexOf(href) > -1) {
-        //defined = true;
-      //}
-    //}
-    //if (defined) {
-      //cb();
-    //} else {
-      //setTimeout(function() {
-        //ss.onloadcssdefined(cb);
-      //});
-    //}
-  //};
-  //ss.onloadcssdefined(function() {
-    //ss.media = media || 'all';
-  //});
-  //return ss;
-//}
