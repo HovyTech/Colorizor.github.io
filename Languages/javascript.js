f0cfe5d4d6facdd89e0f75fa22f74b9498fbf9dd
Alexander Hovy <colorizor@gmail.com>
2015-10-24 16:50:59 +0200

    Update javascript.js

--- Languages/javascript.js
+++ Languages/javascript.js
@@ -10,7 +10,7 @@ $(function() {
   var jsChar = /((?!(function)\s)([\w]+)\(\)(?=(.*?){)|((\$|\.([\w]+))(.*?)|([\w]+))\(|(?!(.*?){)\)|\)(?=,))/igm;
   
   $.each($('pre'), function() {
-    if ($(this).attr('language') = 'css') {
+    if ($(this).attr('language').val().toLowerCase().toString() = 'css') {
       //-------------------------Get Text
       var jsStr = $(this).html();
     
