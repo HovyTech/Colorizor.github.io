fb630b70998cc7bfebddaf7d5cf490168a89d4e1
Alexander Hovy <colorizor@gmail.com>
2015-10-25 12:18:17 +0200

    Update FullScript.js

--- Files/FullScript.js
+++ Files/FullScript.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
       $('#background-cover').css({
         'display': 'block'
       });
-    } else if ($(window).scrollTop() + $(window).height() < $(document).height() - 100) {
+    } else if ($(document).height() - 100 > $(window).scrollTop() + $(window).height()) {
       $('#background-cover').css({
         'display': 'none'
       });
