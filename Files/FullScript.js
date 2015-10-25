92c52beeebb7131951609c5ae7cf1ed88a0369d3
Alexander Hovy <colorizor@gmail.com>
2015-10-25 11:46:20 +0200

    Update FullScript.js

--- Files/FullScript.js
+++ Files/FullScript.js
@@ -1,3 +1,31 @@
+//--------------------------------------------------LOADING MAIN
+function colorizor() {
+  getFacebookCount();
+  getTwitterCount();
+  getPinterestCount();
+  getLinkedInCount();
+  
+  if (screen.width < 1024) {
+    $('pre').width(screen.width - 40);
+  }
+  
+  $('span[id="all-code"]').click(function() {
+    var range, selection;
+
+    if (window.getSelection && document.createRange) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents($(this)[0]);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    } else if (document.selection && document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText($(this)[0]);
+      range.select();
+    }
+  });
+}
+
 //--------------------------------------------------FIXED BACKGROUND ON OFF
 function loadBackground() {
   var headerHeight;
@@ -27,33 +55,6 @@ function loadBackground() {
       $('#background-cover').css({
         'display': 'none'
       });
-    }
-  });
-}
-
-function colorizor() {
-  getFacebookCount();
-  getTwitterCount();
-  getPinterestCount();
-  getLinkedInCount();
-  
-  if (screen.width < 1024) {
-    $('pre').width(screen.width - 40);
-  }
-  
-  $('span[id="all-code"]').click(function() {
-    var range, selection;
-
-    if (window.getSelection && document.createRange) {
-      selection = window.getSelection();
-      range = document.createRange();
-      range.selectNodeContents($(this)[0]);
-      selection.removeAllRanges();
-      selection.addRange(range);
-    } else if (document.selection && document.body.createTextRange) {
-      range = document.body.createTextRange();
-      range.moveToElementText($(this)[0]);
-      range.select();
     }
   });
 }
