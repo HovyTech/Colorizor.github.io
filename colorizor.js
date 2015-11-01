d9c4f11ff565142c5f1f66fdfd13dbcf1b931bc0
Alexander Hovy <colorizor@gmail.com>
2015-11-01 16:00:50 +0200

    Update colorizor.js

--- colorizor.js
+++ colorizor.js
@@ -1,5 +1,53 @@
 (function() {
   //----------------------------------------------------------------------------------------------------------
+  //--------------------------------------------------LoadJS--------------------------------------------------
+  //----------------------------------------------------------------------------------------------------------
+  function loadJS(src, cb) {
+    var ref = window.document.getElementsByTagName('script')[0];
+    var script = window.document.createElement('script');
+    script.src = src;
+    script.async = true;
+    ref.parentNode.insertBefore(script, ref);
+    if (cb && typeof(cb) === 'function') {
+      script.onload = cb;
+    }
+    return script;
+  }
+  //----------------------------------------------------------------------------------------------------------
+  //--------------------------------------------------LoadCSS-------------------------------------------------
+  //----------------------------------------------------------------------------------------------------------
+  function loadCSS(href, before, media, callback) {
+    var ss = window.document.createElement('link');
+    var ref = before || window.document.getElementsByTagName('script')[0];
+    var sheets = window.document.styleSheets;
+    ss.rel = 'stylesheet';
+    ss.href = href;
+    ss.media = 'only x';
+    if (callback) {
+      ss.onload = callback;
+    }
+    ref.parentNode.insertBefore(ss, ref);
+    ss.onloadcssdefined = function(cb) {
+      var defined;
+      for (var i = 0; i < sheets.length; i++) {
+        if (sheets[i].href && sheets[i].href.indexOf(href) > -1) {
+          defined = true;
+        }
+      }
+      if (defined) {
+        cb();
+      } else {
+        setTimeout(function() {
+          ss.onloadcssdefined(cb);
+        });
+      }
+    };
+    ss.onloadcssdefined(function() {
+      ss.media = media || 'all';
+    });
+    return ss;
+  }
+  //----------------------------------------------------------------------------------------------------------
   //-----------------------------------------Load jQuery If Not Present---------------------------------------
   //----------------------------------------------------------------------------------------------------------
   if (typeof jQuery == 'undefined') {
@@ -10,54 +58,6 @@
   //----------------------------------------------------------------------------------------------------------
   var waitForLoad = function() {
     if (typeof jQuery != 'undefined') {
-      //----------------------------------------------------------------------------------------------------------
-      //--------------------------------------------------LoadJS--------------------------------------------------
-      //----------------------------------------------------------------------------------------------------------
-      function loadJS(src, cb) {
-        var ref = window.document.getElementsByTagName('script')[0];
-        var script = window.document.createElement('script');
-        script.src = src;
-        script.async = true;
-        ref.parentNode.insertBefore(script, ref);
-        if (cb && typeof(cb) === 'function') {
-          script.onload = cb;
-        }
-        return script;
-      }
-      //----------------------------------------------------------------------------------------------------------
-      //--------------------------------------------------LoadCSS-------------------------------------------------
-      //----------------------------------------------------------------------------------------------------------
-      function loadCSS(href, before, media, callback) {
-        var ss = window.document.createElement('link');
-        var ref = before || window.document.getElementsByTagName('script')[0];
-        var sheets = window.document.styleSheets;
-        ss.rel = 'stylesheet';
-        ss.href = href;
-        ss.media = 'only x';
-        if (callback) {
-          ss.onload = callback;
-        }
-        ref.parentNode.insertBefore(ss, ref);
-        ss.onloadcssdefined = function(cb) {
-          var defined;
-          for (var i = 0; i < sheets.length; i++) {
-            if (sheets[i].href && sheets[i].href.indexOf(href) > -1) {
-              defined = true;
-            }
-          }
-          if (defined) {
-            cb();
-          } else {
-            setTimeout(function() {
-              ss.onloadcssdefined(cb);
-            });
-          }
-        };
-        ss.onloadcssdefined(function() {
-          ss.media = media || 'all';
-        });
-        return ss;
-      }
       //----------------------------------------------------------------------------------------------------------
       //-----------------------------------------------Load Language----------------------------------------------
       //----------------------------------------------------------------------------------------------------------
