f9de7314e4888be4fcc1e9235e47b7644009021c
Alexander Hovy <colorizor@gmail.com>
2015-11-01 23:07:22 +0200

    Update javascript.js

--- Languages/javascript.js
+++ Languages/javascript.js
@@ -16,7 +16,7 @@
   ];
   //----------------------------------------------------JAVASCRIPT
   var javascript = [
-    ['<span id="comment">$&</span>', /\\\/\\\/([\s\S]*?)$/igm],
+    ['<span id="comment">$&</span>', /\\\/\\\/([\s\S]*?)(?=\<\/span\>)/igm],
     ['<span id="value">$&</span>', /(\\\"(.*?)\\\"|\\\'(.*?)\\\')/igm],
     ['<span id="parameter">$&</span>', /(?!\\\$\\\()([\w]+)(?=\\\)\\\.)/igm],
     ['<span id="reserved">$&</span>', /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/igm],
@@ -53,10 +53,8 @@
       str = str.replace(javascript[a][1], javascript[a][0]);
     }
     //----------------------------------------------------REMOVE
-    //Fix
-    str = str.replace(/\\\<\\\/span\\\>/igm, '\<\/span\>');
     //Comment
-    str = str.replace(/\\\/\\\/([\s\S]*?)$/igm, function(rep) {
+    str = str.replace(/\\\/\\\/([\s\S]*?)/igm, function(rep) {
       return rep.replace(/(\<span(.*?)\>|\<\/span\>)/igm, '');
     });
     //String
