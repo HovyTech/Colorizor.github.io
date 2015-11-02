fb3f69575d262ce3d2ce2620dfe0c364c130c8db
Alexander Hovy <colorizor@gmail.com>
2015-11-02 12:53:05 +0200

    Update javascript.js

--- Languages/javascript.js
+++ Languages/javascript.js
@@ -16,7 +16,7 @@
   ];
   //----------------------------------------------------JAVASCRIPT
   var javascript = [
-    ['<span id="comment">$&</span>', /\\\/\\\/([\s\S]*?)(?=\<\/span\>)/igm],
+    ['<span id="comment">$&</span>', /\\\/\\\/([\s\S]*?)$/igm],
     ['<span id="value">$&</span>', /(\\\"(.*?)\\\"|\\\'(.*?)\\\')/igm],
     ['<span id="parameter">$&</span>', /(?!\\\$\\\()([\w]+)(?=\\\)\\\.)/igm],
     ['<span id="reserved">$&</span>', /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/igm],
