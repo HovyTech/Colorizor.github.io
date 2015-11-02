3b529d73c922784ae1d8c0b1a6022220bef9eb13
Alexander Hovy <colorizor@gmail.com>
2015-11-02 20:30:06 +0200

    Update swift.js

--- Languages/swift.js
+++ Languages/swift.js
@@ -65,7 +65,7 @@
     }
     //----------------------------------------------------REMOVE
     //Comment
-    str = str.replace(/((\\\/\\\/)([\s\S]*?)|\<span\sid\=\"comment\"\>)(?=\<\/span\>$)/igm, function(rep) {
+    str = str.replace(/(\<span\sid\=\"comment\"\>)(?=\<\/span\>$)/igm, function(rep) {
       return rep.replace(/(\<span([\s\S]*?)\>|\<\/span\>)/igm, '');
     });
     //String
