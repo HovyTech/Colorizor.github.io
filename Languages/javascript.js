673fc6c85f1910f3de8eba70f941043c274272b1
Alexander Hovy <colorizor@gmail.com>
2015-11-02 16:51:47 +0200

    Update javascript.js

--- Languages/javascript.js
+++ Languages/javascript.js
@@ -25,15 +25,15 @@
     ['<span id="units">$&</span>', /([^\D])([\d.]*?)(em|ex|%|px|cm|mm|in|pt|pc|ch|rem|vh|vw|vmin|vmax)/igm]
   ];
   //----------------------------------------------------JAVASCRIPT
-  var javascript = [ 
-    ['<span id="character">$&</span>', /(\\[\[\]\{\}\+\=\-\;\$\(\)\&\!\?\,\.\d])+/igm],
+  var javascript = [
     ['<span id="comment">$&</span>', /\\\/\\\/([\s\S]*?)$/igm],
-    ['<span id="selector">$&</span>', /(\\\.([\w]+))+/igm],
     ['<span id="value">$&</span>', /(\\\'|\\\")([\s\S]*?)(\\\'|\\\")/igm],
     ['<span id="reserved">$&</span>', /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/igm],
     ['<span id="reserved">$&</span>', /\b(alert|all|anchor|anchors|area|assign|blur|button|checkbox|clearInterval|clearTimeout|clientInformation|close|closed|confirm|constructor|crypto|decodeURI|decodeURIComponent|defaultStatus|document|element|elements|embed|embeds|encodeURI|encodeURIComponent|escape|event|fileUpload|focus|form|forms|frame|innerHeight|innerWidth|layer|layers|link|location|mimeTypes|navigate|navigator|frames|frameRate|hidden|history|image|images|offscreenBuffering|open|opener|option|outerHeight|outerWidth|packages|pageXOffset|pageYOffset|parent|parseFloat|parseInt|password|plugin|prompt|propertyIsEnum|radio|reset|screenX|screenY|scroll|secure|select|self|setInterval|setTimeout|status|submit|taint|text|textarea|top|unescape|untaint|window)\b/igm],
     ['<span id="parameter">$&</span>', /\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\b/igm],
-    ['<span id="parameter">$&</span>', /\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\b/igm]
+    ['<span id="parameter">$&</span>', /\b(onblur|onclick|onerror|onfocus|onkeydown|onkeypress|onkeyup|onmouseover|onload|onmouseup|onmousedown|onsubmit)\b/igm],
+    ['<span id="selector">$&</span>', /\\\.([\w]+)/igm],
+    ['<span id="character">$&</span>', /(\\[\[\]\{\}\+\=\-\;\$\(\)\&\!\?\,\.\d])+/igm]
   ];
   
   //------------------------------------------------------------------------------------------------------------
@@ -80,6 +80,7 @@
     //------------------------------------------------------------------------------------------------------------
     //----------------------------------------------------SAVE----------------------------------------------------
     //------------------------------------------------------------------------------------------------------------
+alert(str)
     $(this).html(str);
   });
   
