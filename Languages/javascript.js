ee0a530dd48a60ca63f8691fe6274414c9a5bb86
Alexander Hovy <colorizor@gmail.com>
2015-11-02 13:03:47 +0200

    Update javascript.js

--- Languages/javascript.js
+++ Languages/javascript.js
@@ -17,7 +17,7 @@
   //----------------------------------------------------JAVASCRIPT
   var javascript = [
     ['<span id="comment">$&</span>', /\\\/\\\/([\s\S]*?)$/igm],
-    ['<span id="value">$&</span>', /(\\\"(.*?)\\\"|\\\'(.*?)\\\')/igm],
+    ['<span id="value">$&</span>', /(\\\'|\\\")([\s\S]*?)(\\\'|\\\")/igm],
     ['<span id="parameter">$&</span>', /(?!\\\$\\\()([\w]+)(?=\\\)\\\.)/igm],
     ['<span id="reserved">$&</span>', /\b(abstract|arguments|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)\b/igm],
     ['<span id="reserved">$&</span>', /\b(Array|Date|eval|function|hasOwnProperty|Infinity|isFinite|isNaN|isPrototypeOf|length|Math|NaN|name|Number|Object|prototype|String|toString|undefined|valueOf)\b/igm],
@@ -54,7 +54,7 @@
     }
     //----------------------------------------------------REMOVE
     //Comment
-    str = str.replace(/\\\/\\\/([\s\S]*?)$/igm, function(rep) {
+    str = str.replace(/\\\/\\\/([\s\S]*?)\<\/span\>/igm, function(rep) {
       return rep.replace(/(\<span(.*?)\>|\<\/span\>)/igm, '');
     });
     //String
